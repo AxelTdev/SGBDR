dfbc4cc9ff47c3eecfb5524feed981124e375f96
Axel <Axel@10.30.46.197>
2019-10-10 20:02:48 +0200

    remplissage fichier

--- src/Code/diskManager/DiskManager.java
+++ src/Code/diskManager/DiskManager.java
@@ -1,54 +1,60 @@
 package Code.diskManager;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
 import Code.pages.PageId;
+import Code.util.Constants;
 
 public class DiskManager {
-	private int compteur_page = 1;
-	private static DiskManager  instance = null;
-	
+	private static int compteur_page = 1;
+	private static DiskManager instance = null;
+
 	private DiskManager() {
-		
+
 	}
 
 	public static DiskManager getInstance() {
-		if(instance==null) {
-			instance=new DiskManager ();
+		if (instance == null) {
+			instance = new DiskManager();
 		}
 		return instance;
-		}
-	
-	public static void CreateFile (int fileIdx) {
-		try {
-			//DB/mets le fichier  dans le repertoire DB
-			RandomAccessFile file = new RandomAccessFile("DB/Data_"+fileIdx+".rf", "rw");
-		} catch (FileNotFoundException e) {			
-			e.printStackTrace();
-		}
 	}
-	
-	public static PageId AddPage (int fileIdx) {
-		
-		PageId p = new PageId(fileIdx,compteur_page);
+
+	public static void CreateFile(int fileIdx) {
+
+		// DB/mets le fichier dans le repertoire DB
+		File file = new File("DB/Data_" + fileIdx +".rf");
+
+	}
+
+	public static PageId AddPage(int fileIdx) throws IOException {
+
+		PageId p = new PageId(fileIdx, compteur_page);
 		compteur_page++;
+		byte[] buffer = new byte[Constants.pageSize];
+		
+			RandomAccessFile r = new RandomAccessFile("DB/Data_" + p.fileIdx + ".rf", "rw");
+			r.write(buffer);
+			r.close();
+		
 		return p;
 	}
-	
-	public static void ReadPage (PageId p, ByteBuffer b) {
-		try (RandomAccessFile r=new RandomAccessFile ("DB/Data_"+p.fileIdx+".rf","r");) {
-			            
-			
+
+	public static void ReadPage(PageId p, ByteBuffer b) {
+		try (RandomAccessFile r = new RandomAccessFile("DB/Data_" + p.fileIdx + ".rf", "r");) {
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 	
-	public static void WritePage (PageId p, ByteBuffer b) {
-		
+	public static void WritePage(PageId p, ByteBuffer b) {
+
 	}
 }
